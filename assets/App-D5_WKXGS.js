83c6fd10a4bcd21b2d1c8584c0bb0809213517d1
Nurbek <nurbek.malikov@nu.edu.kz>
2024-05-10 02:10:37 +0500

    building

--- assets/App-D5_WKXGS.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/react-spline-BRYiIB4_.js","assets/index-DA47_GuE.js","assets/index-Y-To4cEB.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{Y as e,_ as s,j as a,N as n}from"./index-DA47_GuE.js";e.lazy(()=>s(()=>import("./react-spline-BRYiIB4_.js"),__vite__mapDeps([0,1,2])));const i="/HeartUpLogo.svg";function t(){return a.jsxs(a.Fragment,{children:[a.jsx(n,{linkText:"Sign In",className:"navbar--fixed",navigateTo:"/login"}),a.jsx("main",{className:"main-container",children:a.jsx("div",{className:"main-container__text",children:a.jsxs("h1",{children:[a.jsx("img",{src:i,alt:""}),"eart",a.jsx("span",{children:"Up"})]})})})]})}export{t as default};
