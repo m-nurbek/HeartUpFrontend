4dcdedce72c169649bc42bc9271284a4e7dd2b6f
Nurbek <nurbek.malikov@nu.edu.kz>
2024-05-10 02:10:37 +0500

    building

---
+++ dist/assets/App-BjYjbM0g.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./react-spline-Dz5HD1na.js","./index-FIaJa4oy.js","./index-BcBFpPqg.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{Y as e,_ as r,j as a,N as n}from"./index-FIaJa4oy.js";e.lazy(()=>r(()=>import("./react-spline-Dz5HD1na.js"),__vite__mapDeps([0,1,2]),import.meta.url));const s=""+new URL("../HeartUpLogo.svg",import.meta.url).href;function i(){return a.jsxs(a.Fragment,{children:[a.jsx(n,{linkText:"Sign In",className:"navbar--fixed",navigateTo:"/login"}),a.jsx("main",{className:"main-container",children:a.jsx("div",{className:"main-container__text",children:a.jsxs("h1",{children:[a.jsx("img",{src:s,alt:""}),"eart",a.jsx("span",{children:"Up"})]})})})]})}export{i as default};
